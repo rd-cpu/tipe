ef368cab9a086e66e9fa4d1d33691a18728b4325
rd-cpu <rdxbzh@gmail.com>
2025-10-28 23:39:12 +0100

    des trucs

--- crack/simulation_temporelles.py
+++ crack/simulation_temporelles.py
@@ -1,11 +1,14 @@
-# from crackEGEC import *
-# import csv
-# import time
+from module.crackEGEC import *
+import csv
+import time
+import numpy as np
 
 
+CEstand = CourbeElliptique(2,0,2,49993)
 
-# def duree_crack(CE,N):
-#     tab_sk = [randint(0,CE.o-1) for i in range(N)]
-#     tab_pk = [generate_PK(s,P,CE) for s in ls]
-#     tab_temps_crack = [0]*N
-#     for pk in tab_pk:
+def duree_crack(CE,N):
+    tab_sk = np.random.uniform(0,CE.o-1,N)
+    tab_P = np.array([point_random(CE) for i in range(N)])
+    tab_pk = generate_PK(tab_sk,P,)
+    tab_temps_crack = numpy.array([0]*N)
+    
