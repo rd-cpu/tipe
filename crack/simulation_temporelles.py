e2150b3fc4e898e2384a2aeb9e7b81cfa61cd672
rd-cpu <rdxbzh@gmail.com>
2025-10-29 15:31:03 +0100

    rien je renvoie des trucs dans simulation_temporel les

--- crack/simulation_temporelles.py
+++ crack/simulation_temporelles.py
@@ -6,21 +6,20 @@ import numpy as np
 
 CEstand = CourbeElliptique(2,0,2,6482753)
 
-def duree_crack(CE,N=10000):
+def duree_crack(CE,algo_crack,N=10000):
     o = ordre_max(CE)
     tab_sk = np.random.randint(1,o, size=N)
     tab_P = np.array([point_random(CE) for i in range(N)])
     tab_pk = generate_PK(tab_sk,tab_P,CE)
     tab_temps = np.array([0]*N)
-    print("tab_sk :", tab_sk)
-    print("tab_pk :", tab_pk)
     tab_temps = np.array([0]*N)
     for i in range(N):
         start = time.perf_counter()
-        sk_cracké = crack_force_brute(tab_pk[i])
+        sk_cracké = algo_crack(tab_pk[i])
         end = time.perf_counter()
         tab_temps[i] = end - start # en s
         print(i)
     temps_moyen = np.mean(tab_temps)
     u_temps = np.std(tab_temps,ddof=1)
     print("le temps nécessaire pour cracker la CE est", temps_moyen,"s +-",u_temps)
+    return tab_sk,tab_P,tab_temps,temps_moyen,u_temps
