b05ea4112bbc2cbde567cbc3cbc39e12b1076948
rd-cpu <rdxbzh@gmail.com>
2025-12-04 16:11:53 +0100

    un commit t'as un peu moins peur

---
+++ crack/module/algo_crack/CE.c
@@ -0,0 +1,70 @@
+#include "CE.h"
+
+static inline uint64_t modmul(uint64_t a, uint64_t b, uint64_t mod) {
+    return (uint64_t)(((__uint128_t)a * (__uint128_t)b) % mod);
+}
+
+uint64_t modpow(uint64_t base, uint64_t exp, uint64_t mod) {
+    uint64_t res = 1;
+    base %= mod;
+    while (exp) { 
+        if (exp & 1) res = modmul(res, base, mod);
+        base = modmul(base, base, mod);
+        exp >>= 1;
+    }
+    return res;
+}
+
+uint64_t fp(Point P) {
+    uint64_t p = P.CE->p;
+    return (3 * modmul(P.x, P.x, p) + P.CE->a) % p;  // Modularité !
+}
+
+uint64_t inv_mod(uint64_t e, uint64_t p) {
+    int64_t a = (int64_t)p, b = (int64_t)(e % p);
+    int64_t x0 = 1, x1 = 0;
+    int64_t y0 = 0, y1 = 1;
+    int64_t q, r, tmp;
+
+    while (b != 0) {
+        q = a / b; r = a % b;
+        tmp = x1; x1 = x0 - q * x1; x0 = tmp;
+        tmp = y1; y1 = y0 - q * y1; y0 = tmp;
+        a = b; b = r;
+    }
+    if (a != 1) return 0;
+    if (x0 < 0) y0 += p;
+    return (uint64_t) y0;
+}
+
+Point etoile(Point P1, Point P2) {
+    Point P3 = {0};  // Initialisation !
+    uint64_t p = P1.CE->p, a = P1.CE->a;
+    P3.CE = P1.CE;
+
+    if (P1.infini) return P2;
+    if (P2.infini) return P1;
+    
+    if (P1.x == P2.x) {
+        if (P1.y != P2.y || P1.y == 0) {
+            P3.infini = 1;
+            return P3;
+        }
+        uint64_t lambda_num = (modmul(3, modmul(P1.x, P1.x, p), p) + a) % p;
+        uint64_t lambda_den = (2 * P1.y) % p;
+        uint64_t inv_den = inv_mod(lambda_den, p);
+        if (!inv_den) { P3.infini = 1; return P3; }
+        P3.x = (modmul(inv_den, lambda_num, p) * inv_den % p * inv_den % p + p - 2 * P1.x + p) % p; // CORRIGÉ
+        P3.y = (modmul(inv_den, lambda_num, p) * (P1.x + p - P3.x) % p + p - P1.y) % p; // CORRIGÉ
+    } else {
+        uint64_t lambda_num = ((P2.y + p - P1.y) % p);
+        uint64_t lambda_den = ((P2.x + p - P1.x) % p);
+        uint64_t inv_den = inv_mod(lambda_den, p);
+        if (!inv_den) { P3.infini = 1; return P3; }
+        uint64_t lambda = modmul(lambda_num, inv_den, p);
+        P3.x = (modmul(lambda, lambda, p) + p - P1.x + p - P2.x) % p;
+        P3.y = (modmul(lambda, (P1.x + p - P3.x) % p, p) + p - P1.y) % p; // CORRIGÉ
+    }
+    P3.infini = 0;
+    return P3;
+}
--- crack/module/algo_crack/CE.h
+++ crack/module/algo_crack/CE.h
@@ -7,5 +7,19 @@
 static inline uint64_t modmul(uint64_t a, uint64_t b, uint64_t mod);
 uint64_t modpow(uint64_t base, uint64_t exp, uint64_t mod);
 
+typedef struct{
+    uint64_t a, b, p;
+} CE;
 
+typedef struct {
+    uint64_t x, y, fp;
+    int infini;
+    CE* CE;
+} Point;
+
+uint64_t fp(Point P);
+
+uint64_t inv_mod(uint64_t e, uint64_t p);
+
+Point etoile(Point P1, Point P2);
 #endif
